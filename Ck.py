6ea415bdbbbeda7f282caf1dd8f5d5253e1e1e6e
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2024-06-21 04:20:13 +0300

    Update Ck.py

--- Ck.py
+++ Ck.py
@@ -1,2 +1,19 @@
+# Import necessary libraries
 import pyximport
-pyximport.mahadi()
+pyximport.install()
+
+# Create a Cython file
+mahadi = """
+def reverse_string(str):
+    return str[::-1]
+"""
+
+# Write the Cython code to a .pyx file
+with open("reverse_string.pyx", "w") as f:
+    f.write(mahadi)
+
+# Compile the Cython file
+import reverse_string
+
+# Test the function
+print(reverse_string.reverse_string("Hello, Cython!"))
